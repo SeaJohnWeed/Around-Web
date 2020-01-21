0c2c91b83e124b5b58744501ead90d74d7abaa3b
SeaJohnWeed <jkjohn0624@gmail.com>
2020-01-20 21:04:59 -0800

    refactor directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Topbar} from "./Topbar"
-import {Register} from "./Register"
-import './App.css';
+import {Topbar} from "../components/Topbar"
+import {Register} from "../components/Register"
+import '../styles/App.css';
 
 function App() {
   return (

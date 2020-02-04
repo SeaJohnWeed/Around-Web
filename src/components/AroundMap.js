cafe1025b67a80863d40e0b16fb6c707f9404786
Kang Jiang <kangjiang@gatech.edu>
2020-02-03 19:41:46 -0800

    google map marker toggle

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -3,17 +3,41 @@ import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
+  Marker,
+  InfoWindow
 } from "react-google-maps";
 
 class NormalAroundMap extends React.Component {
+
+  state = {
+    isOpen: false,
+  }
+
+  onToggleOpen = () => {
+    this.setState(({isOpen}) => ({
+        isOpen: !isOpen,
+      }))
+  }
+
   render() {
     return (
       <GoogleMap
         defaultZoom={8}
-        defaultCenter={{ lat: 122, lng: 37}}
+        defaultCenter={{ lat: 34.052235, lng: -118.243683 }}
       >
-        <Marker position={{ lat: 122, lng: 37}} />
+        <Marker
+          position={{ lat: 34.052235, lng: -118.243683 }}
+          onClick={this.onToggleOpen}
+        >
+          {
+            this.state.isOpen ? (
+              <InfoWindow onCloseClick={this.onToggleOpen}>
+                <div>hello</div>
+              </InfoWindow>
+            ) :
+              null
+          }
+        </Marker>
       </GoogleMap>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -163,7 +163,7 @@ export class Home extends React.Component {
           <AroundMap
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
+            containerElement={<div style={{ height: `600px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
           />
         </TabPane>
